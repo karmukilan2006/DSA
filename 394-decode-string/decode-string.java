f641302811161fe954b6eedaa6bf01560be8d17d
karmukilan2006 <162773723+karmukilan2006@users.noreply.github.com>
2026-01-08 21:19:22 +0530

    Time: 3 ms (22.93%) | Memory: 43.1 MB (14.99%) - LeetSync

--- 394-decode-string/decode-string.java
+++ 394-decode-string/decode-string.java
@@ -1,43 +1,41 @@
 class Solution {
     public String decodeString(String s) {
-        Stack<String> stringStack=new Stack<>();
-        Stack<Integer> countStack=new Stack<>();
-        int n=s.length();
-
+        Stack<Integer> countstack=new Stack<>();
+        Stack<String> stringstack=new Stack<>();
         int num=0;
         String curr="";
-
-        for(int i=0;i<n;i++)
+        for(int i=0;i<s.length();i++)
         {
-            char ch = s.charAt(i);
-
-            if (Character.isDigit(ch)) {
-                num = num * 10 + (ch - '0');
+            char ch=s.charAt(i);
+            if((Character.isDigit(ch)))
+            {
+                 num = num * 10 + (ch - '0');
             }
+            else if(ch=='[')
+            {
+                countstack.push(num);
+                stringstack.push(curr);
+                num=0;
+                curr="";
 
-            else if (ch == '[') {
-                countStack.push(num);
-                stringStack.push(curr);
-                num = 0;
-                curr = "";
             }
-
-            else if (Character.isLetter(ch)) {
+            else if(Character.isLetter(ch))
+            {
                 curr = curr + ch;
             }
-            
-            else { 
-                int k = countStack.pop();
-                String prev = stringStack.pop();
-
-                String temp = "";
-                for (int j = 0; j < k; j++) {
-                    temp = temp + curr;
+            else
+            {
+                int k=countstack.pop();
+                String prev=stringstack.pop();
+                String temp="";
+                for(int j=0;j<k;j++)
+                {
+                    temp=temp+curr;
                 }
+                curr=prev+temp;
+                
 
-                curr = prev + temp;
             }
-
         }
         return curr;
 
