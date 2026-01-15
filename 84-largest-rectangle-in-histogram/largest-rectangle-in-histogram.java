e9b19c2a60f599aba2a67794127742161e93d8fb
karmukilan2006 <162773723+karmukilan2006@users.noreply.github.com>
2026-01-15 12:06:13 +0530

    Time: 59 ms (75.75%) | Memory: 76.3 MB (89.16%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
@@ -9,18 +9,21 @@ class Solution {
 
             int currHeight = (i == n) ? 0 : heights[i];
 
-            while (!stack.isEmpty() && currHeight < heights[stack.peek()]) {
+            while (!stack.isEmpty() && currHeight <= heights[stack.peek()]) 
+            {
 
                 int height = heights[stack.pop()];
                 int width;
-
-                if (stack.isEmpty()) {
+                if (stack.isEmpty()) 
+                {
                     width = i;
-                } else {
+                } 
+                else 
+                {
                     width = i - stack.peek() - 1;
                 }
-
                 maxArea = Math.max(maxArea, height * width);
+
             }
 
             stack.push(i);
