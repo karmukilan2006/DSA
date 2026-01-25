019116fd392ed7fdcbd1cf240a403a040e62e5b8
karmukilan2006 <162773723+karmukilan2006@users.noreply.github.com>
2026-01-25 14:37:48 +0530

    Time: 0 ms (100.00%) | Memory: 43.8 MB (24.40%) - LeetSync

--- 101-symmetric-tree/symmetric-tree.java
+++ 101-symmetric-tree/symmetric-tree.java
@@ -24,11 +24,10 @@ class Solution {
         if (left == null && right == null) return true;
 
         // one null
-        if (left == null || right == null) return false;
+        if (left == null || right == null || left.val != right.val) return false;
 
         // values must match and subtrees must be mirror
-        return (left.val == right.val)
-                && isMirror(left.left, right.right)
+        return isMirror(left.left, right.right)
                 && isMirror(left.right, right.left);
     }
 }
