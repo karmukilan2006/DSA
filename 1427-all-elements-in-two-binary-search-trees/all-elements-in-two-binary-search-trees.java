9ee5539601a42e8b158b9d6a2023f40bdd10ce76
karmukilan2006 <162773723+karmukilan2006@users.noreply.github.com>
2026-02-14 16:42:57 +0530

    Time: 17 ms (81.66%) | Memory: 49.6 MB (8.52%) - LeetSync

---
+++ 1427-all-elements-in-two-binary-search-trees/all-elements-in-two-binary-search-trees.java
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    ArrayList<Integer> list=new ArrayList<>();
+    public List<Integer> getAllElements(TreeNode root1, TreeNode root2) {
+        solve(root1);
+        solve(root2);
+        Collections.sort(list);
+        return list;
+    }
+    public void solve(TreeNode root)
+    {
+        if(root==null)
+        {
+            return;
+        }
+        
+        solve(root.left);
+        list.add(root.val);
+        solve(root.right);
+    }
+}
