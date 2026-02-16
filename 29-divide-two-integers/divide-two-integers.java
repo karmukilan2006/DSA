02b0587ee61f56613cf4e755a40cd8b253e49d89
karmukilan2006 <162773723+karmukilan2006@users.noreply.github.com>
2026-02-16 23:23:51 +0530

    Time: 1 ms (90.29%) | Memory: 42.9 MB (12.07%) - LeetSync

--- 29-divide-two-integers/divide-two-integers.java
+++ 29-divide-two-integers/divide-two-integers.java
@@ -10,8 +10,8 @@ class Solution {
         if ((dividend > 0 && divisor < 0) || (dividend < 0 && divisor > 0))
             sign = false;
 
-        long n = Math.abs((long) dividend);
-        long d = Math.abs((long) divisor);
+        long n = Math.abs((long)dividend);
+        long d = Math.abs((long)divisor);
 
         long res = 0;
 
