169a1097e1081aac7ae9bc5c19300247a6a47673
karmukilan2006 <162773723+karmukilan2006@users.noreply.github.com>
2026-02-18 11:20:52 +0530

    Time: 997 ms (10.49%) | Memory: 9.6 MB (7.03%) - LeetSync

---
+++ 136-single-number/single-number.c
@@ -0,0 +1,26 @@
+void solve(int* nums,int numsSize)
+{
+    for(int i=0;i<numsSize-1;i++)
+    {
+        for(int j=0;j<numsSize-i-1;j++)
+        {
+            if(nums[j]>nums[j+1])
+            {
+                int temp=nums[j];
+                nums[j]=nums[j+1];
+                nums[j+1]=temp;
+            }
+        }
+    }
+}
+int singleNumber(int* nums, int numsSize) {
+    solve(nums,numsSize);
+    for(int i=0;i<numsSize-1;i+=2)
+    {
+        if(nums[i]!=nums[i+1])
+        {
+            return nums[i];
+        }
+    }
+    return nums[numsSize-1];
+}
