28952483f669c220d2ffda86364d4621c17b8d32
karmukilan2006 <162773723+karmukilan2006@users.noreply.github.com>
2026-02-23 23:30:22 +0530

    Time: 100 ms (60.91%) | Memory: 51.5 MB (13.08%) - LeetSync

---
+++ 204-count-primes/count-primes.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int countPrimes(int n) {
+        if(n==0 || n==1)
+        {
+            return 0;
+        }
+        boolean isprime[] =new boolean[n+1];
+        for (int i = 2; i < n; i++) {
+            isprime[i] = true;
+        }
+        for (int i = 2; i * i < n; i++) 
+        {
+             if (isprime[i]) 
+            {
+                for (int j = i * i; j < n; j += i)
+                {
+                    isprime[j] = false;
+                }
+            }
+       
+        }
+
+        int count = 0;
+        for (boolean prime : isprime) 
+        {
+            if (prime) count++;
+        }
+        return count;
+
+
+
+    }
+}
