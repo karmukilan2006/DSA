cb79da2a793e79b2179429934bc7a83479bfa2c5
karmukilan2006 <162773723+karmukilan2006@users.noreply.github.com>
2026-03-31 21:43:24 +0530

    Time: 7 ms (5.09%) | Memory: 45.1 MB (72.19%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.java
+++ 496-next-greater-element-i/next-greater-element-i.java
@@ -8,14 +8,12 @@ class Solution {
             stack.push(nums1[i]);
         }
         int q=0;
-
         while(!stack.isEmpty())
         {
             int top=stack.pop();
-             int flag=1;
+            int flag=1;
             for(int i=0;i<nums2.length;i++)
             {
-
                 if(top==nums2[i])
                 {
                     for(int j=i+1;j<nums2.length;j++)
