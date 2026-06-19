486daf87a28df8eeff053f52000e16f7a9c995c5
karmukilan2006 <162773723+karmukilan2006@users.noreply.github.com>
2026-06-19 11:56:25 +0530

    Time: 0 ms (100.00%) | Memory: 44.8 MB (88.72%) - LeetSync

--- 98-validate-binary-search-tree/validate-binary-search-tree.java
+++ 98-validate-binary-search-tree/validate-binary-search-tree.java
@@ -1,30 +1,20 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
- class Solution {
+class Solution {
     public boolean isValidBST(TreeNode root) {
         return validate(root, Long.MIN_VALUE, Long.MAX_VALUE);
     }
 
     private boolean validate(TreeNode node, long min, long max) {
-        if (node == null) return true;
+        if(node == null)
+        {
+            return true;
+        }
 
-        if (node.val <= min || node.val >= max) return false;
+        if(node.val <= min || node.val >= max)
+        {
+            return false;
+        }
 
         return validate(node.left, min, node.val) &&
                validate(node.right, node.val, max);
     }
 }
-
